6c39ddaff5f176840ed157c26993bff8d4d91ecf
Uni <tenbitsrwlm@gmail.com>
2025-10-26 14:06:51 +0300

    Структуризация кода

---
+++ sandbox/src/main/java/ru/uni/geometry/Geometry.java
@@ -0,0 +1,16 @@
+package ru.uni.geometry;
+
+import ru.uni.geometry.figures.Rectangle;
+import ru.uni.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(7.0);
+        Square.printSquareArea(5.0);
+        Square.printSquareArea(3.0);
+
+        Rectangle.printRectangleArea(3.0, 5.0);
+        Rectangle.printRectangleArea(7.0, 9.0);
+    }
+
+}
---
+++ sandbox/src/main/java/ru/uni/geometry/figures/Square.java
@@ -0,0 +1,13 @@
+package ru.uni.geometry.figures;
+
+public class Square {
+    public static void printSquareArea(double a) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", a, squareArea(a));
+        System.out.println(text);
+
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
+    }
+}
