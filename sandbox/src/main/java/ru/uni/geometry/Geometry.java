e66830277d424d4195b9e1c0465365f1e20f3d70
Uni <tenbitsrwlm@gmail.com>
2025-12-10 19:40:59 +0300

    7.1

--- sandbox/src/main/java/ru/uni/geometry/Geometry.java
+++ sandbox/src/main/java/ru/uni/geometry/Geometry.java
@@ -3,14 +3,22 @@ package ru.uni.geometry;
 import ru.uni.geometry.figures.Rectangle;
 import ru.uni.geometry.figures.Square;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(new Square(7.0));
-        Square.printSquareArea(new Square(5.0));
-        Square.printSquareArea(new Square( 3.0));
-
-        Rectangle.printRectangleArea(3.0, 5.0);
-        Rectangle.printRectangleArea(7.0, 9.0);
+        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
+//        for(Square square: squares){
+//            Square.printSquareArea(square);
+//        }
+        //Исп функции вместо цикла
+        // Consumer<Square> print = (square)-> {Square.printSquareArea(square);};
+        //Альт вариант написания
+        Consumer<Square> print = Square::printSquareArea;
+        squares.forEach(print);
+//        Rectangle.printRectangleArea(3.0, 5.0);
+//        Rectangle.printRectangleArea(7.0, 9.0);
     }
 
 }
