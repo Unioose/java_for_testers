ba4d27f471d156df4b6b0658cec7740791322ca9
Uni <tenbitsrwlm@gmail.com>
2025-12-13 17:10:01 +0300

    7.3

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -12,6 +12,7 @@ import org.hibernate.cfg.Configuration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HibernateHelper extends  HelperBase{
 
@@ -32,11 +33,13 @@ public class HibernateHelper extends  HelperBase{
     }
 
     static List<GroupData> convertList(List<GroupRecord> records){
-        List<GroupData> result = new ArrayList<>();
-        for (var record : records){
-            result.add(convert(record));
-        }
-        return result;
+       return records.stream().map(HibernateHelper::convert).collect(Collectors.toList());
+
+//        List<GroupData> result = new ArrayList<>();
+//        for (var record : records){
+//            result.add(convert(record));
+//        }
+//        return result;
     }
 
     private static GroupData convert(GroupRecord record) {
@@ -88,11 +91,12 @@ public class HibernateHelper extends  HelperBase{
     }
 
     static List<ContactData> convertContactList(List<ContactRecord> records){
-        List<ContactData> result = new ArrayList<>();
-        for (var record : records){
-            result.add(convert(record));
-        }
-        return result;
+        return records.stream().map(HibernateHelper::convert).collect(Collectors.toList());
+//        List<ContactData> result = new ArrayList<>();
+//        for (var record : records){
+//            result.add(convert(record));
+//        }
+//        return result;
     }
 
     public List<ContactData> getContactsInGroup(GroupData group) {
